79e22fc8b40fe2cbf45c1c836d915b12f0dd1f1b
Roberta O <vitortk@hotmail.com>
2022-01-26 16:36:07 -0800

    testing in the client with the register button

---
+++ client/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      placeholder="email" />
+    <br>
+    <input
+      type="password"
+      name="password"
+      placeholder="password" />
+    <br>
+    <button
+      @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    register () {
+      console.log('register was clicked')
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+</style>
